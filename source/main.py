6c945f8c1438c62ffb0ac89d0ddd11297bee0b9c
Cjoota <claudiobastardo_13@hotmail.com>
2025-07-20 13:57:13 -0400

    version 1.0.0

--- source/main.py
+++ source/main.py
@@ -61,7 +61,7 @@ def limpar_cache():
 	for doc in Path("pdf_temp").glob("*.pdf"):
 		doc.unlink()
 	
-ft.app(target=Main,view=ft.AppView.WEB_BROWSER, host="192.168.0.245",port=53712,assets_dir="assets")
+ft.app(target=Main,view=ft.AppView.WEB_BROWSER, host="0.0.0.0",port=53712,assets_dir="assets")
 
 
 
