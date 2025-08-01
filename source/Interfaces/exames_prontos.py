f923704402603019f6f0daa51c49f7a7c18c5a44
Cjoota <claudiojacomo30@gmail.com>
2025-08-01 15:26:46 -0400

    Hotfix: exames_prontos2

--- source/Interfaces/exames_prontos.py
+++ source/Interfaces/exames_prontos.py
@@ -243,7 +243,7 @@ class Documentos:
             self.page.update()
         
         def abrirdoc(self, idx) -> None:
-            requesite = f"{idx[0]} {str(idx[1]).replace(" ", "-")} {str(idx[2]).replace(" ", "-")} {idx[3]} {str(idx[4]).replace(":","-")}.xlsx".replace("/", "-")
+            requesite = f"{str(idx[2]).replace(" ","-")} {str(idx[3]).replace("/","-")} {str(idx[4]).replace(":","-")}"
             print(requesite)
             try:
                 documentosdir = Path("documentos_gerados")
